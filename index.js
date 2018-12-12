eabfe90b1b12da3cb57720f82bea6f9a633aaddd
Dan <spag1024@hotmail.com>
2018-12-12 16:30:05 -0600

    worked with functions

---
+++ index.js
@@ -0,0 +1,42 @@
+//console.log("Hello World!");
+//my comment
+
+//let name = 'Dan';
+
+//console.log(name);
+
+//let firstName = 'Dan';
+//let lastName = 'Hansen';
+
+//let interestRate = .3;
+//interestRate = 1;
+
+//console.log(interestRate);
+
+//let name = 'Dan'; //string literal
+//let age = 5; //number literal
+//let isApproved = true; //Boolean literal
+//let firstName = undefined; //explicitly not typed
+//firstName = 'Dan';
+//firstName = 1;
+//firstName = null;
+//console.log(typeof firstName);
+
+//let person = {
+//    name: 'Dan',
+//    age: 35
+//};
+
+////let firstName = 'John';
+//person.name = firstName;
+//console.log(person.name);
+
+//let colors = ['red', 'blue'];
+
+//console.log(colors);
+
+function greet(firstName) {
+    console.log("Hello " + firstName);
+};
+
+greet('Dan');
