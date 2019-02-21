8314454c0561c31d2bb91609adf3535e3ba8f0e7
Dan H <spag1024@hotmail.com>
2019-02-21 04:15:54 -0800

    just experimenting

--- index.js
+++ index.js
@@ -1,45 +1,26 @@
-//console.log("Hello World!");
-//my comment
-
-//let name = 'Dan';
-
-//console.log(name);
-
-//let firstName = 'Dan';
-//let lastName = 'Hansen';
-
-//let interestRate = .3;
-//interestRate = 1;
-
-//console.log(interestRate);
-
-//let name = 'Dan'; //string literal
-//let age = 5; //number literal
-//let isApproved = true; //Boolean literal
-//let firstName = undefined; //explicitly not typed
-//firstName = 'Dan';
-//firstName = 1;
-//firstName = null;
-//console.log(typeof firstName);
-
-//let person = {
-//    name: 'Dan',
-//    age: 35
-//};
-
-////let firstName = 'John';
-//person.name = firstName;
-//console.log(person.name);
-
-//let colors = ['red', 'blue'];
-
-//console.log(colors);
-
-//function greet(firstName) {
-  //  console.log("Hello " + firstName);
-//};
-
-//greet('Dan');
 
 
-greet('Dan');
+// BlackJack by Dan Hansen
+
+// let card1 = "Ace of Spades";
+// let card2 = "Ten of Hearts";
+
+
+// console.log("You are dealt: ");
+// console.log(" " + card1);
+// console.log(" " + card2);
+
+// let values = [];
+
+// console.log(values, typeof(values));
+
+// let cities = ['New York', 'Los Angeles', 'Chicago'];
+
+// console.log(cities);
+
+let values = [];
+values.push(44, 45, 46);
+
+results = values.pop();
+
+console.log(values, results);
